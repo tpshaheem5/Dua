72197f84ac036ab621f38bac16af59ec722a2b8c
tpshaheem5 <shaheemtp5@gmail.com>
2023-08-03 12:59:15 +0530

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './component/Home';
+
+export const myContaxt = createContext();
 
 function App() {
+  const [alldata, setAlldata] = useState([]); // Initialize data with an empty array
+  const [category,setCategory]=useState([])
+  const data= {
+    alldata,setAlldata,
+    category,setCategory
+  }
+
+  useEffect(() => {
+    fetch('https://api.npoint.io/a5309d721d856bb517d5/')
+      .then((response) => response.json())
+      .then((jsonData) => {
+         console.log('API response:', jsonData);
+        setAlldata(jsonData.categories);
+          // console.log("sub_category",jsonData.sub_categories  );
+          setCategory(jsonData.sub_categories
+            )
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        setAlldata([]); // Handle any error by setting data to an empty array
+      });
+  }, []);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <myContaxt.Provider value={data}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </myContaxt.Provider>
     </div>
   );
 }
---
+++ src/component/Duas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Duas = () => {
+
+  return (
+
+    
+    <div>Duas</div>
+  )
+}
+
+export default Duas
