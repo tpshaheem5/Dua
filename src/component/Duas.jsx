cd128424e6198447a3b5b6f561cd202200f42f51
tpshaheem5 <shaheemtp5@gmail.com>
2023-08-06 16:06:09 +0530

    3 commit

--- src/component/Duas.jsx
+++ src/component/Duas.jsx
@@ -1,65 +1,49 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { myContaxt } from '../App'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { myContaxt } from "../App";
+import { useParams } from "react-router-dom";
 
 const Duas = () => {
-  const {duas} = useContext(myContaxt)
+  const { duas } = useContext(myContaxt);
 
-  const [vajamnams,setVajanams]=useState([])
-  
+  const [vajamnams, setVajanams] = useState([]);
 
-  const {duasid}= useParams()
+  const { duasid } = useParams();
 
-  
-  useEffect(()=>{
+  useEffect(() => {
+    console.log("paramas id", duasid);
 
-    console.log("paramas id",duasid)
-
-    duaHandle(duasid)
-  },[])
+    duaHandle(duasid);
+  }, []);
 
   // *********HANDLE DUAS**************
 
-  const duaHandle=(...dua_ids)=>{
-    
-    console.log(" duas inside handler",dua_ids);
-    
-    const pass_id = parseInt(dua_ids)
-    
-    console.log("typeof ids",typeof(pass_id));
-
-    // const flattened_dua_ids = dua_ids.flat();
-    console.log("dua_ids",dua_ids);
-    const filterd_duas = duas.filter((item) =>dua_ids.includes(item.id));
-    console.log("filtered_duas",filterd_duas);
-    setVajanams(filterd_duas)
-
-    console.log("vajanam after sett",vajamnams);
-    
-
-
-  }
-
+  const duaHandle = (dua_ids) => {
+    console.log("duas inside handler", dua_ids);
+  
+    const pass_ids = dua_ids.split(",").map(Number);
+  
+    console.log("pass_ids", pass_ids);
+  
+    const filtered_duas = duas.filter((item) => pass_ids.includes(item.id));
+    console.log("filtered_duas", filtered_duas);
+    setVajanams(filtered_duas);
+  
+    console.log("vajanam after set", vajamnams);
+  };
+  
 
   return (
-
-    
-     <div className='dua_data'>
-
-    {vajamnams.map((item)=>{
-       return(
-        <>
-        
-             <h3 key={item.id}>
-               {item.dua}
-             </h3>
+    <div className="dua_data">
+      {vajamnams.map((item) => {
+        return (
+          <>
+            <h3 key={item.id}>{item.dua}</h3>
             <h3>{item.trans}</h3>
-        </>
-         )
-  })}
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
-</div>
-  )
-}
-
-export default Duas
+export default Duas;
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,104 +1,36 @@
- import React, { useContext, useEffect, useState} from 'react';
- import { myContaxt } from '../App';
-//  import
+import React, { useContext, useEffect } from "react";
+import { myContaxt } from "../App";
 
- import'./Home.css'
-import { useNavigate } from 'react-router-dom';
-
-// function Home() {
-//   const { alldata,category } = useContext(myContaxt);
-//   console.log("Home data",alldata);
-
-//   const [prayers, setPreyers] =useState([])
-
-//   const filterHandle =(...category_id)=>{
-     
-//      console.log("category_id",category_id);
-
-    
-   
-    
-
-//     const filterd_data= category.filter((item)=>category_id.includes(item.id))
-//     console.log("filterd_data",filterd_data);
-//     setPreyers(filterd_data)
-//     // console.log("FILTRD DATA",prayers);
-    
-
-//   }
-
-//   useEffect(() => {
-   
-//     console.log("PRAYERS",prayers);
-//      }, [prayers]);
-
-
-//   return (
-//       <div className='main_container'>  
-  
-//          <div className='main_data' style={{}}>
-//                <h1>Homee</h1>
-//                {alldata.map((e) => (
-//                 <div key={e.id}>
-//                   <button onClick={()=>filterHandle(e.sub_cats)}>{e.title }</button>
-
-          
-//                       {/* <h2>{e.sub_cats}</h2>  */}
-//                  </div>
-       
-//                ))}
-       
-//           </div>
-
-//            <div className='sub_data'>
-//               {prayers.map((item)=>{
-//                 return(
-//                   <h3>{item.title}</h3>
-//                 )
-//                })}
-
-//             </div>
-//      </div>
-
-//   );
-// }
-
-// export default Home;
+import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const { alldata, category,prayers,setPrayers } = useContext(myContaxt);
-  const nav= useNavigate()
+  const { alldata, category, prayers, setPrayers } = useContext(myContaxt);
+  const nav = useNavigate();
 
+  // ************TO FILTER THE SUB CATEGORY ARRAY**********
 
-  // console.log("Home data", alldata);
-
-  // const [prayers, setPreyers] = useContext();
-
-// ************TO FILTER THE SUB CATEGORY ARRAY**********
-  
   const prayersFilterHandle = (...category_id) => {
     console.log("category_id", category_id);
 
-    console.log("category data inside filter handle",category);
+    console.log("category data inside filter handle", category);
 
     const flattened_category_ids = category_id.flat();
 
     const filterd_data = category.filter((item) =>
-    flattened_category_ids.includes(item.id)
+      flattened_category_ids.includes(item.id)
     );
     console.log("filterd_data", filterd_data);
     setPrayers(filterd_data);
   };
-
-  
 
   useEffect(() => {
     console.log("PRAYERS", prayers);
   }, [prayers]);
 
   return (
-    <div className='main_container'>
-      <div className='main_data'>
+    <div className="main_container">
+      <div className="main_data">
         <h1>Duas</h1>
         {alldata.map((e) => (
           <div key={e.id}>
@@ -109,22 +41,22 @@ function Home() {
         ))}
       </div>
 
-      <div className='sub_data' style={{display:'flex',justifyContent:'space-between'}}>
+      <div
+        className="sub_data"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
         {prayers.map((item) => {
-          return(
-          <>
-          <h3 onClick={()=>nav(`./Duas/${item.duas}`)} key={item.id}>{item.title}</h3>
-          
-          </>
-          
-          )
-        
+          return (
+            <>
+              <h3 onClick={() => nav(`./Duas/${item.duas}`)} key={item.id}>
+                {item.title}
+              </h3>
+            </>
+          );
         })}
-        
       </div>
     </div>
   );
 }
 
 export default Home;
-
